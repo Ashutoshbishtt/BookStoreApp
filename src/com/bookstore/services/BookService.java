d29f7b9a4042ccc20c6a20a8a5053bc3050ff28e
Ashutosh Bisht <ashu9872180@gmail.com>
2024-05-21 23:03:38 +0530

    Added BookService class with methods to add books and retrieve all books.

--- src/com/bookstore/services/BookService.java
+++ src/com/bookstore/services/BookService.java
@@ -1,4 +1,20 @@
 package com.bookstore.services;
 
+import com.bookstore.models.Book;
+import java.util.List;
+import java.util.ArrayList;
+
 public class BookService {
+    // List to store books
+    private List<Book> books = new ArrayList<>();
+
+    // Method to add a book to the list
+    public void addBook(Book book) {
+        books.add(book);
+    }
+
+    // Method to retrieve all books from the list
+    public List<Book> getAllBooks() {
+        return books;
+    }
 }
